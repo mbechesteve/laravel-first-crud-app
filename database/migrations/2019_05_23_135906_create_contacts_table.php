86459d2cd27a67818856edd02921ddb71c0e7d3b
mbeche stephen <stephen.mbeche@squaddigital.com>
2019-05-23 17:01:28 +0300

    adding extra fields on your contacts table

--- database/migrations/2019_05_23_135906_create_contacts_table.php
+++ database/migrations/2019_05_23_135906_create_contacts_table.php
@@ -13,9 +13,15 @@ class CreateContactsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contacts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+       Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
             $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('job_title');
+            $table->string('city');   
+            $table->string('country');            
         });
     }
 
