af9ce9081435ac97876583fff7a9043ba3f0e367
mbeche stephen <stephen.mbeche@squaddigital.com>
2019-05-23 17:23:47 +0300

    contacts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -79,7 +79,8 @@ class ContactController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = Contact::find($id);
+        return view('contacts.edit', compact('contact'));  
     }
 
     /**
@@ -91,7 +92,22 @@ class ContactController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'email'=>'required'
+        ]);
+
+        $contact = Contact::find($id);
+        $contact->first_name =  $request->get('first_name');
+        $contact->last_name = $request->get('last_name');
+        $contact->email = $request->get('email');
+        $contact->job_title = $request->get('job_title');
+        $contact->city = $request->get('city');
+        $contact->country = $request->get('country');
+        $contact->save();
+
+        return redirect('/contacts')->with('success', 'Contact updated!');
     }
 
     /**
@@ -102,6 +118,9 @@ class ContactController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = Contact::find($id);
+        $contact->delete();
+
+        return redirect('/contacts')->with('success', 'Contact deleted!');
     }
 }
